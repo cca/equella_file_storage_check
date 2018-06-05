3e974f3342b5bd473bd543f4d2f11eaaa5cd677c
phette23 <phette23@gmail.com>
2018-06-04 17:08:02 -0700

    record the staging directory in item XML
    set up necessary to copy staging files in the event that we detect missing ones

--- expert-scripting.js
+++ expert-scripting.js
@@ -2,9 +2,18 @@
 // used to check if an item's staging directory contains the same files as its
 // persistent storage does, use staging API
 if (staging.isAvailable()) {
-    xml.deleteAll('local/staging') // clear out previous results
+    // clear out previous results
+    xml.deleteAll('local/staging')
+    xml.deleteAll('local/stagingDir')
+
     var stagingFilesAndDirs = staging.listFiles("", "**")
-	for(var i = 0; i < stagingFilesAndDirs.size(); i++) {
+	for (var i = 0; i < stagingFilesAndDirs.size(); i++) {
 		xml.add('local/staging', stagingFilesAndDirs.get(i))
 	}
+
+    var stagingFiles = staging.list("/", "*")
+    if (stagingFiles.size() > 0) {
+        // getHandle is technically unsupported but it works
+        xml.add('local/stagingDir', staging.getFileHandle(stagingFiles.get(0)).getHandle().getAbsolutePath())
+    }
 }
